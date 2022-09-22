a839024e8e7c6d6bafb2619a25d298dd1877c6d4
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-09-22 11:17:52 -0500

    created componets and reducers

--- src/index.js
+++ src/index.js
@@ -3,4 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+// Reducers:
+
+const cartReducer = (state=[], action)=>{
+    if (action.type === 'ADD_ITEM'){
+
+    }
+    else if (action.type === 'REMOVE_ITEM'){
+
+    }
+    else if (action.type === 'CLEAR_CART'){
+        return [];
+    }
+
+    return state;
+}
+
+const customer = (state = [], action) => {
+    if (action.type === 'ADD_CUSTOMER'){
+
+    }
+    else if (action.type === 'YEET_CUSTOMER'){
+
+    }
+    return state;
+}
+
+const totalPrice = (state=0, action)=> {
+    if (action.type === 'ADD_PRICE'){
+        return state + action.payload;
+    }
+
+    else if (action.type === 'SUBTRACT_PRICE'){
+        return state - action.payload;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        cartReducer,
+        customer,
+        totalPrice
+    }),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
