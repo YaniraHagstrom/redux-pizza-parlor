3dc64a218280eeb696247c561d91fc5177612b18
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-09-23 10:10:56 -0500

    working on header conditional render

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
 import { useSelector } from "react-redux";
+// import { useLocation } from "react-router-dom";
+// import { useState, useEffect } from 'react';
 
 export default function Header(){
+    const [total, setTotal] = useState('')
+
     const totalPrice = useSelector(store=> store.totalPrice)
+    // const location = useLocation();
+    console.log(location.hash);
+    // console.log(totalPrice)
+    
+    // const showTotal = ()=> {
+    //     if (location.hash === '#/' || location.hash === '#/customer'){
+    //         setTotal(`$${totalPrice}`);
+    //     }
+    //     else{
+    //         setTotal('')
+    //     }
+    // }
 
     return (
         <header className='App-header'>
             <h1 className='App-title'>Prime Pizza</h1>
-            <h2>${totalPrice}</h2>
+            {location.hash === '#/' || location.hash === '#/customer'  ? <h2>${totalPrice}</h2> : '' }
         </header>
     )
 }
+
